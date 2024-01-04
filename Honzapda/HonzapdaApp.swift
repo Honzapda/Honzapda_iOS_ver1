76adecb3d36a4da4de454a8b308a15ae0ff0c56e
youz2me <kynhun20@gachon.ac.kr>
2024-01-04 19:45:18 +0900

    [Build] Initial commit

---
+++ Honzapda/HonzapdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonzapdaApp.swift
+//  Honzapda
+//
+//  Created by YOUJIM on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HonzapdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
