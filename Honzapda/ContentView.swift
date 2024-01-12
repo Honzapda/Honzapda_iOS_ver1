908743676843e54834a3c0da28f79b91d8b85f3b
hj1487 <huiju_lee@naver.com>
2024-01-13 03:47:48 +0900

    test
    github-testing

--- Honzapda/ContentView.swift
+++ Honzapda/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Github Branch Testing")
+            Text("Changed Setting")
         }
         .padding()
     }
